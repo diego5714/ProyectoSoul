b7bf2f9b779178d16687b2057808d503eb31441f
Ferna <fernandopaniagua@ug.chile.cl>
2023-09-30 11:40:47 -0300

    Enemigo Basico
    Enemigo simple que cambia de dirección si no hay piso o si hay muralla. Para ello se creo un nivel de prueba donde se ve su comportamiento
    No posee animación pues es solo algo básico (no sabemos si usaremos otro sprite como enemigo)

---
+++ Enemys/Enemy.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+@export var SPEED = 100
+@export var GRAVITY = 1000
+@export var ACCELERATION = 1500
+@export var JUMP_SPEED = 700
+var direction = Vector2.RIGHT
+@onready var pivote = $Pivote
+@onready var floor_ray = $Pivote/FloorRay
+@onready var side_ray = $Pivote/SideRay
+
+
+func _physics_process(delta):
+	velocity.x = move_toward(velocity.x, pivote.scale.x * SPEED, ACCELERATION*delta)
+	
+	if not is_on_floor():
+		velocity.y += GRAVITY*delta
+		
+	if is_on_floor() and not floor_ray.is_colliding():
+		pivote.scale.x *= -1
+	if is_on_floor() and side_ray.is_colliding():
+		pivote.scale.x *= -1
+	move_and_slide()
+	
