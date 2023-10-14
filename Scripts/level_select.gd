9af359c130b53372b6c35badf80ce5b1a7029c33
diego5714 <diego.soto@ing.uchile.cl>
2023-10-14 01:45:43 -0300

    Archivos faltantes

---
+++ Scripts/level_select.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@export var Nivel1: PackedScene
+@export var Nivel2: PackedScene
+@export var Nivel3: PackedScene
+@export var Nivel4: PackedScene
+
+@export var Main: PackedScene
+
+@onready var Boton1 := %Boton1
+@onready var Boton2 := %Boton2
+@onready var Boton3 := %Boton3
+@onready var Boton4 := %Boton4
+
+func _ready():
+	Boton1.pressed.connect(_on_boton_1_pressed)
+	Boton2.pressed.connect(_on_boton_2_pressed)
+
+func _on_boton_1_pressed():
+	get_tree().change_scene_to_packed(Main)
+	pass
+
+
+func _on_boton_2_pressed():
+	pass
