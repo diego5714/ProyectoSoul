1cf33fa3a165ddec1dbad47ef9536739cb067c2d
Ferna <fernandopaniagua@ug.chile.cl>
2023-10-15 12:56:38 -0300

    Escenas y arreglos nivel 3
    Escenas creadas Boton y Escalera la idea es que al apretar boton la escalera 2 aparesca, pero no pude hacer el codigo.
    Escena LLave actualizada
    Nueva Escena Estacas (sin codigo)
    Falta poner a los personajes en el nivel 3 para provar como va

---
+++ Scenes/Escalera.gd
@@ -0,0 +1,14 @@
+extends Area2D
+#
+#var button_push = false
+#
+#func _ready() -> void:
+#	var button = $"../Button" #Referencia al boton emisor 
+#	button.button_pushed.connect(_appear_ladder())
+#
+#
+#
+#func _appear_ladder():
+#	button_push = true
+#	get_child(0).show()
+#
