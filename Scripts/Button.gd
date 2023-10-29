ed0a992763343d160088056daec69cceaddee9ed
Ferna <fernandopaniagua@ug.chile.cl>
2023-10-29 16:48:47 -0300

    Boton y Puente
    Falta poder desactivar o activar colision puente

--- Scripts/Button.gd
+++ Scripts/Button.gd
@@ -6,4 +6,5 @@ signal button_pushed
 func _on_body_entered(body: Node2D) -> void:
 	if body.get_name() == "PlayerA" or body.get_name() == "PlayerB":
 		emit_signal("button_pushed")
+		print("boton")
 	
