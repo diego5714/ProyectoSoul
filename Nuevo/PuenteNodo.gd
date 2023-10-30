6045c6a4227df0937fa43f6b84edc1d3e45479b6
Ferna <fernandopaniagua@ug.chile.cl>
2023-10-30 12:23:40 -0300

    script puentenodo

---
+++ Nuevo/PuenteNodo.gd
@@ -0,0 +1,19 @@
+extends Node2D
+var mostrar = false
+var posicion = Vector2(-1984,416)
+
+func _process(delta: float) -> void:
+	if (mostrar == true):
+		instanciar_puente()
+		mostrar = false
+	
+func _on_button_button_pushed() -> void:
+	mostrar = true
+	
+func instanciar_puente():
+	var puente = load("res://Scenes/Puente_Madera.tscn").instantiate()
+	self.add_child(puente)
+	puente.global_position=posicion
+	puente.scale= Vector2(2,2)
+	
+	
