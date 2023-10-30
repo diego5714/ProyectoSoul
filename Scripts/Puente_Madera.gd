3da7eba673aa77bdd5338cb5762fba06fe49f05c
Ferna <fernandopaniagua@ug.chile.cl>
2023-10-30 12:09:14 -0300

    puente funcional

--- Scripts/Puente_Madera.gd
+++ Scripts/Puente_Madera.gd
@@ -1,24 +1,29 @@
 extends StaticBody2D
-var mostrar = false
-
-@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-@onready var tile_map: TileMap = $TileMap
-
-func _ready():
-	#collision_shape_2d.set_deferred("disabled",true)
-	#tile_map.set_deferred("collision_visibility_mode",2)
-	
-	self.hide()
-	
-func _process(_delta: float) -> void:
-	collision_and_show()
-	
-func collision_and_show():
-	if mostrar == true:
-		self.show()
-		self.set_collision_layer_value(1,true)
-
-
-func _on_button_button_pushed() -> void:
-	mostrar = true
-	
+#var mostrar = false
+#
+#@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+#@onready var tile_map: TileMap = $TileMap
+#@onready var puente: StaticBody2D = $"."
+#
+#func _ready():
+#	#collision_shape_2d.set_deferred("disabled",true)
+#	#tile_map.set_deferred("collision_visibility_mode",2)
+#	puente.set_collision_layer_value(1,false)
+#	puente.set_collision_mask_value(1,false)
+#
+#	self.hide()
+#
+#func _process(_delta: float) -> void:
+#	collision_and_show()
+#
+#func collision_and_show():
+#	if mostrar == true:
+#		self.show()
+#		puente.set_collision_layer_value(1,true)
+#		puente.set_collision_mask_value(1,true)
+#
+#
+#
+#func _on_button_button_pushed() -> void:
+#	mostrar = true
+#
