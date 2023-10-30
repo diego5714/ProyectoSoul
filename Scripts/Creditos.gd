88ed8b80eaad957137280c544f7962962306f870
diego5714 <diego.soto@ing.uchile.cl>
2023-10-30 02:18:59 -0300

    Mejoras, animaciones y sonidos de menu (WIP)

--- Scripts/Creditos.gd
+++ Scripts/Creditos.gd
@@ -2,3 +2,7 @@ extends Control
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Main_Menu.tscn")
+
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Assets_Credits.tscn")
--- Scripts/Main_Menu.gd
+++ Scripts/Main_Menu.gd
@@ -9,12 +9,21 @@ func _ready() -> void:
 	start.pressed.connect(_on_start_pressed)
 	exit.pressed.connect(_on_exit_pressed)
 	credits.pressed.connect(_on_credits_pressed)
+	%Button_Animations.play("RESET")
 
 func _on_start_pressed():
+	%ClickSound.play()
+	%Button_Animations.play("Start_Pressed")
+	await %Button_Animations.animation_finished
+	
 	get_tree().change_scene_to_file("res://Scenes/Level_Selector.tscn")
 
 func _on_exit_pressed():
+	$SFX/ClickSound.play()
+	await $SFX/ClickSound.finished
 	get_tree().quit()
 
 func _on_credits_pressed():
+	$SFX/ClickSound.play()
+	await $SFX/ClickSound.finished
 	get_tree().change_scene_to_file("res://Scenes/Creditos.tscn")
