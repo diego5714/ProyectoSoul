99311ab94d3e5e0a87dcc02ef57f728d7675e998
Diego5714 <dsotocaneo@gmail.com>
2023-10-31 00:16:02 -0300

    Animacion para la llave

--- Scripts/Llave.gd
+++ Scripts/Llave.gd
@@ -2,6 +2,10 @@ extends Area2D
 
 signal puerta_llave
 
+func _ready():
+	$Sprite_Animation.play("Shine")
+	$Position_Animation.play("wobble")
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.get_name() == "PlayerA" or body.get_name() == "PlayerB":
 		emit_signal("puerta_llave")
