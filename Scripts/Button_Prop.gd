41c00b066055a549dd8ae2489f02b98dd9121b3e
Diego5714 <dsotocaneo@gmail.com>
2023-11-30 21:00:16 -0300

    WIP Tutorial y fix para asset de sonido de gato coleccionable roto

--- Scripts/Button_Prop.gd
+++ Scripts/Button_Prop.gd
@@ -4,15 +4,17 @@ signal button_pushed
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var collision_shape_2d = $CollisionShape2D
 
+@export var persistente: bool = true
+
 
 func _ready():
-	monitoring = true
-	collision_shape_2d.disabled = false
+	set_deferred("monitoring", true)
+	collision_shape_2d.set_deferred("disabled", false)
 	
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
-	collision_shape_2d.disabled = true
+	collision_shape_2d.set_deferred("disabled", true)
 	
 	if body.get_name() == "Player":
 		emit_signal("button_pushed")
